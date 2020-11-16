1013fb2e2fe67136cf9d941c103a059d8d4417ef
Vilandas <d00218436@student.dkit.ie>
2020-11-16 20:23:08 +0000

    Adds menu

--- CA2_Jack_Maguire_Vilandas_Morrissey/Program.cs
+++ CA2_Jack_Maguire_Vilandas_Morrissey/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using static CA2_Jack_Maguire_Vilandas_Morrissey.Enums;
 
 namespace CA2_Jack_Maguire_Vilandas_Morrissey
 {
@@ -8,30 +9,48 @@ namespace CA2_Jack_Maguire_Vilandas_Morrissey
 
     public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             new Program();
         }
 
+        #region Fields
+
+        private string inKey = "\n)> ";
+        private double gravity = 9.81;
+        private double time = 5;
+        private double steps = 0.1;
+        private double radius = 0.05;
+        private double density = 7800;
+        private Vector3 position = new Vector3(2, -3, 6);
+        private Vector3 velocity = new Vector3(-5, 14, 2);
+        private Vector3 spin = new Vector3(-10d / 3d, -5d / 3d, 10d / 3d);
+
+        private double fluidDensity = 80;
+        private double dragCoeff = 0.1;
+        private Vector3 flowRate = new Vector3(2, 3, 0);
+
+        #endregion
+
+        #region Properties
+
+        public double Gravity { get; set; }
+        public double Time { get; set; }
+        public double Steps { get; set; }
+        public double Radius { get; set; }
+        public double Density { get; set; }
+        public Vector3 Position { get; set; }
+        public Vector3 Velocity { get; set; }
+        public Vector3 Spin { get; set; }
+        public double FluidDensity { get; set; }
+        public double DragCoeff { get; set; }
+        public Vector3 FlowRate { get; set; }
+
+        #endregion
+
         public Program()
         {
-            double gravity = 9.81;
-            double time = 5;
-            double h = 0.1;
-            double radius = 0.05;
-            double density = 7800;
-            Vector3 position = new Vector3(2, -3, 6);
-            Vector3 velocity = new Vector3(-5, 14, 2);
-            Vector3 spin = new Vector3(-10d / 3d, -5d / 3d, 10d / 3d);
-
-            double fluidDensity = 80;
-            double dragCoeff = 0.1;
-            Vector3 flowRate = new Vector3(2, 3, 0);
-
-            Demo(gravity,
-                time, h, radius, density,
-                position, velocity, spin,
-                fluidDensity, dragCoeff, flowRate);
+            Start();
         }
 
         public void Demo(double gravity,
@@ -43,24 +62,23 @@ namespace CA2_Jack_Maguire_Vilandas_Morrissey
             double mass = density * volume;
 
             Vector3 va = Vector3.Subtract(velocity, flowRate);
-            Vector3 unitVa = Vector3.Normalise(va);
             double vaLength = va.Length();
 
             Vector3 fGravity = ForceGravity(mass, gravity);
-            Vector3 fDrag = ForceDrag(fluidDensity, radius, dragCoeff, vaLength, unitVa);
-            Vector3 fMagnus = ForceMagnus(fluidDensity, radius, vaLength, spin, va, unitVa);
+            Vector3 fDrag = ForceDrag(fluidDensity, radius, dragCoeff, vaLength, va);
+            Vector3 fMagnus = ForceMagnus(fluidDensity, radius, vaLength, spin, va);
 
-            Debug.WriteLine("fGravity: " + fGravity);
-            Debug.WriteLine("fDrag: " + fDrag);
-            Debug.WriteLine("fMagnus: " + fMagnus);
+            Console.WriteLine("\nfGravity: " + fGravity);
+            Console.WriteLine("fDrag: " + fDrag);
+            Console.WriteLine("fMagnus: " + fMagnus);
 
             Vector3 fNet = fGravity + fDrag + fMagnus;
-            Debug.WriteLine("\nfNet: " + fNet);
+            Console.WriteLine("\nfNet: " + fNet);
 
-            Vector3 acceleration = Vector3.Multiply(fNet, 1d/mass);
-            Debug.WriteLine("acceleration: " + fNet);
+            Vector3 acceleration = Vector3.Multiply(fNet, 1d / mass);
+            Console.WriteLine("acceleration: " + fNet);
 
-            Eulers(position, velocity, acceleration, time, h);
+            Rk4(position, velocity, acceleration, time, h);
         }
 
         //F̅g = -mgk̂
@@ -70,14 +88,15 @@ namespace CA2_Jack_Maguire_Vilandas_Morrissey
         }
 
         //F̅d = ||F̅d|| * F̂d = [½ * P * A * Cd * ||Va||²][-V̂a]
-        public Vector3 ForceDrag(double fluidDensity, double radius, double dragCoeff, double vaLength, Vector3 unitVa)
+        public Vector3 ForceDrag(double fluidDensity, double radius, double dragCoeff, double vaLength, Vector3 va)
         {
             double fd = (1d / 2d) * fluidDensity * (Math.PI * Math.Pow(radius, 2)) * dragCoeff * Math.Pow(vaLength, 2);
+            Vector3 unitVa = Vector3.Normalise(va);
             return Vector3.Multiply(unitVa, -fd);
         }
 
         //F̅m = ||F̅m|| * F̂m
-        public Vector3 ForceMagnus(double fluidDensity, double radius, double vaLength, Vector3 spin, Vector3 va, Vector3 unitVa)
+        public Vector3 ForceMagnus(double fluidDensity, double radius, double vaLength, Vector3 spin, Vector3 va)
         {
             //F̅m = ||F̅m|| * F̂m
             //Fm = d * r * ||R̅|| * ||V̅a||
@@ -95,7 +114,237 @@ namespace CA2_Jack_Maguire_Vilandas_Morrissey
 
         public void Rk4(Vector3 position, Vector3 velocity, Vector3 acceleration, double time, double h)
         {
+        }
 
+
+        public void Start()
+        {
+            bool run = true;
+            while (run)
+            {
+                MenuOptions menuChoice = (MenuOptions)Enum.Parse(typeof(MenuOptions), GetChoice(typeof(MenuOptions)).ToString());
+                switch (menuChoice)
+                {
+                    case MenuOptions.Exit:
+                        run = false;
+                        break;
+                    case MenuOptions.Example_one:
+                        Demo(9.81,
+                            5, 0.1, 0.05, 7800,
+                            new Vector3(2, -3, 6), new Vector3(-5, 14, 2), new Vector3(-10d / 3d, -5d / 3d, 10d / 3d),
+                            80, 0.1, new Vector3(2, 3, 0));
+                        break;
+                    case MenuOptions.Example_two:
+                        Demo(9.81,
+                            5, 0.1, 0.05, 7800,
+                            new Vector3(2, -3, 6), new Vector3(-5, 14, 2), new Vector3(-10d / 3d, -5d / 3d, 10d / 3d),
+                            80, 0.1, new Vector3(2, 3, 0));
+                        break;
+                    case MenuOptions.Example_three:
+                        Demo(9.81,
+                            5, 0.1, 0.05, 7800,
+                            new Vector3(2, -3, 6), new Vector3(-5, 14, 2), new Vector3(-10d / 3d, -5d / 3d, 10d / 3d),
+                            80, 0.1, new Vector3(2, 3, 0));
+                        break;
+                    case MenuOptions.Custom:
+                        Demo(gravity,
+                            time, steps, radius, density,
+                            position, velocity, spin,
+                            fluidDensity, dragCoeff, flowRate);
+                        break;
+                    case MenuOptions.Modify_Custom:
+                        ModifyMenu();
+                        break;
+                }
+            }
+        }
+
+        private void ModifyMenu()
+        {
+            bool run = true;
+
+            while(run)
+            {
+                DisplayCurrentValues();
+                ModifyOptions menuChoice = (ModifyOptions)Enum.Parse(typeof(ModifyOptions), GetChoice(typeof(ModifyOptions)).ToString());
+                
+                if(menuChoice != ModifyOptions.Exit)
+                    Console.WriteLine("\nModifying " + Enums.GetModifyName(menuChoice) + ":");
+
+                switch (menuChoice)
+                {
+                    case ModifyOptions.Exit:
+                        run = false;
+                        break;
+                    case ModifyOptions.Modify_Gravity:
+                        gravity = GetDouble();
+                        break;
+                    case ModifyOptions.Modify_Time:
+                        time = GetDouble();
+                        break;
+                    case ModifyOptions.Modify_Time_Steps:
+                        steps = GetDouble();
+                        break;
+                    case ModifyOptions.Modify_Radius:
+                        radius = GetDouble();
+                        break;
+                    case ModifyOptions.Modify_Density:
+                        density = GetDouble();
+                        break;
+                    case ModifyOptions.Modify_DragCoefficent:
+                        dragCoeff = GetDouble();
+                        break;
+                    case ModifyOptions.Modify_FluidDensity:
+                        fluidDensity = GetDouble();
+                        break;
+                    case ModifyOptions.Modify_Position:
+                        position = GetVector();
+                        break;
+                    case ModifyOptions.Modify_Velocity:
+                        velocity = GetVector();
+                        break;
+                    case ModifyOptions.Modify_Spin:
+                        spin = GetVector();
+                        break;
+                    case ModifyOptions.Modify_FlowRate:
+                        flowRate = GetVector();
+                        break;
+                }
+            }
+        }
+
+        private void DisplayCurrentValues()
+        {
+            Console.WriteLine("\nGravity: " + gravity);
+            Console.WriteLine("Time: " + time);
+            Console.WriteLine("Time-Steps: " + steps);
+
+            Console.WriteLine("Radius: " + radius);
+            Console.WriteLine("Density: " + density);
+            Console.WriteLine("Position: " + position);
+            Console.WriteLine("Velocity: " + velocity);
+            Console.WriteLine("Spin: " + spin);
+
+            Console.WriteLine("Fluid Density: " + fluidDensity);
+            Console.WriteLine("Drag Coefficent: " + dragCoeff);
+            Console.WriteLine("Flow Rate: " + flowRate);
+        }
+
+        /**
+        * Ask user for input and check if it is in the menu choice range.
+        * @return Menu choice number (int)
+        */
+        private object GetChoice(Type enumType)
+        {
+            while (true)
+            {
+                DisplayMenu(enumType);
+                int number = GetInt();
+
+                Array menus = Enum.GetValues(enumType);
+                if (number < 0 || number >= menus.Length)
+                {
+                    Console.WriteLine("\nInvalid number");
+                    continue;
+                }
+                else return menus.GetValue(number);
+            }
+        }
+
+        /**
+        * Display menu options
+        */
+        private void DisplayMenu(Type enumType)
+        {
+            Console.WriteLine("\nInput number to select option: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            string[] names = Enums.GetNames(enumType);
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine("\t" + i + ")" + names[i]);
+            }
+            Console.ResetColor();
+            Console.Write("Enter a number" + inKey);
+        }
+
+        /**
+        * Ask user for input until they enter a valid Integer number
+        * @return integer number
+        */
+        public int GetInt()
+        {
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+            } while (!IsValidNumber(input, false));
+            return Convert.ToInt32(input);
+        }
+
+        /**
+        * Ask user for input until they enter a valid Double number
+        * @return long number
+        */
+        private double GetDouble()
+        {
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+            } while (!IsValidNumber(input, true));
+            return Convert.ToDouble(input);
+        }
+
+        private Vector3 GetVector()
+        {
+            Console.WriteLine("Input X value" + inKey);
+            double x = GetDouble();
+            Console.WriteLine("Input Y value" + inKey);
+            double y = GetDouble();
+            Console.WriteLine("Input Z value" + inKey);
+            double z = GetDouble();
+
+            return new Vector3(x, y, z);
+        }
+
+        /**
+        * Check to see if an input can be parsed to int or double.
+        * @param input String of user inputted numbers/letters
+        * @param isDouble boolean if true, check to see if the input can be parsed to Double instead of Integer.
+        * @return True if the input is numeric and can be parsed. False otherwise.
+        */
+        private bool IsValidNumber(string input, bool isDouble)
+        {
+            if (input == null)
+            {
+                DisplayInvalidInputError();
+                return false;
+            }
+            try
+            {
+                if(isDouble)
+                {
+                    Convert.ToDouble(input);
+                }
+                else Convert.ToInt32(input);
+                return true;
+            }
+            catch (FormatException)
+            {
+                DisplayInvalidInputError();
+                return false;
+            }
+            catch (OverflowException)
+            {
+                DisplayInvalidInputError();
+                return false;
+            }
+        }
+
+        private void DisplayInvalidInputError()
+        {
+            Console.WriteLine("\nInvalid input, numbers only");
+            Console.Write("\nEnter a number" + inKey);
         }
 
     }
